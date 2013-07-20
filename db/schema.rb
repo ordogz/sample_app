367a806109c9c98b7b69eb21778283dc9d359651
Ordog Zoltan <ordogz@gmail.com>
2013-07-20 13:16:26 +0300

    Finish user edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130717170850) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
