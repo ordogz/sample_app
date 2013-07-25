9c5fb23db570e97a831354519ac3e0ff7aa3407f
Ordog Zoltan <ordogz@gmail.com>
2013-07-25 16:31:43 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #
 
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessible :name, :email, :password, :password_confirmation, :admin
   has_secure_password
   before_save :create_remember_token
@@ -22,6 +23,11 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+    
+  end
   private
     
     def create_remember_token
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Authentication" do
     it { should have_selector('h1',    text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
-
+  
   describe "signin" do
     before { visit signin_path }
 
@@ -19,7 +19,7 @@ describe "Authentication" do
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-
+      
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
@@ -28,32 +28,30 @@ describe "Authentication" do
     
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
 
       it { should have_link('Users',    href: users_path) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
 
+      it { should_not have_link('Sign in', href: signin_path) }
+      
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
+
     end
   end
-
+  
   describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
-
+      
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -69,7 +67,23 @@ describe "Authentication" do
           end
         end
       end
+      
+      describe "in the Microposts controller" do
 
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before do
+            micropost = FactoryGirl.create(:micropost)
+            delete micropost_path(micropost)
+          end
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+      
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -81,14 +95,16 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
-
-        describe "visiting user index" do
+        
+        describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
       end
+      
+      
     end
-
+    
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
@@ -104,7 +120,7 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end
     end
-
+  
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
